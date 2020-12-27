1a3ce4f9a64201b430a1ae256e9ee4e12e745d10
mdtaufiq <taufiqul.developer@gmail.com>
2020-12-27 22:03:03 +0600

    2nd commit

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3,5 +3,27 @@
 !function ($) {
   'use strict';
 
-  jQuery(document).on("ready", function () {});
+  jQuery(document).on("ready", function () {
+    $('.wn-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+      prevArrow: "<img class='a-left control-c prev slick-prev' src='assets/images/icon/arrow.svg'>",
+      nextArrow: "<img class='a-right control-c next slick-next' src='assets/images/icon/arrow.svg'>",
+      onInit: function onInit() {
+        $('.slick-active').prev().addClass('prev');
+        $('.slick-active').next().addClass('next');
+      },
+      onBeforeChange: function onBeforeChange() {
+        $('.slick-slide').removeClass('prev next');
+      },
+      onAfterChange: function onAfterChange() {
+        $('.slick-active').prev().addClass('prev');
+        $('.slick-active').next().addClass('next');
+      }
+    });
+  });
 }(jQuery);
--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,29 @@
     jQuery(document).on("ready", function() {
 
 
-       
+        $('.wn-slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            centerMode: true,
+            variableWidth: true,
+            prevArrow:"<img class='a-left control-c prev slick-prev' src='assets/images/icon/arrow.svg'>",
+            nextArrow:"<img class='a-right control-c next slick-next' src='assets/images/icon/arrow.svg'>",
+            onInit: function () {
+                $('.slick-active').prev().addClass('prev');
+                $('.slick-active').next().addClass('next');
+            },
+        
+            onBeforeChange: function () {
+                $('.slick-slide').removeClass('prev next');
+            },
+        
+            onAfterChange: function () {
+                $('.slick-active').prev().addClass('prev');
+                $('.slick-active').next().addClass('next');
+            }
+          });
 
     });
 }(jQuery);
